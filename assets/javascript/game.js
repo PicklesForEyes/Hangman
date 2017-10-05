54b0cc2acd51ed70fd9e9ee17113e4b757cfcce0
Benjamin Lawson <lawbe101@yahoo.com>
2017-10-05 16:46:23 -0400

    changed obj prop names to numbers. Random word function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,209 @@
 var states = {
-  'alabama': 'assets/images/alabama.png',
-  'alaska': 'assets/images/alaska.png',
-  'arizona': 'assets/images/arizona.png',
-  'arkansas': 'assets/images/arkansas.png',
-  'california': 'assets/images/california.png',
-  'colorado': 'assets/images/colorado.png',
-  'connecticut': 'assets/images/connecticut.gif',
-  'deleware': 'assets/images/deleware.png',
-  'florida': 'assets/images/florida.png',
-  'georgia': 'assets/images/georgia.png',
-  'hawaii': 'assets/images/hawaii.png',
-  'idaho': 'assets/images/idaho.png',
-  'illinois': 'assets/images/illinois.png',
-  'indiana': 'assets/images/indiana.png',
-  'iowa': 'assets/images/iowa.png',
-  'kansas': 'assets/images/kansas.gif',
-  'kentucky': 'assets/images/kentucky.png',
-  'louisiana': 'assets/images/louisiana.png',
-  'maine': 'assets/images/maine.png',
-  'maryland': 'assets/images/maryland.png',
-  'massachusetts': 'assets/images/massachusetts.png',
-  'michigan': 'assets/images/michigan.png',
-  'minnesota':'assets/images/minnesota.png',
-  'mississippi': 'assets/images/mississippi.png',
-  'missouri': 'assets/images/missouri.png',
-  'montana': 'assets/images/montana.png',
-  'nebraska': 'assets/images/nebraska.png',
-  'nevada': 'assets/images/nevada.png',
-  'new hampshire': 'assets/images/new_hampshire.gif',
-  'new jersey': 'assets/images/new_jersey.png',
-  'new mexico': 'assets/images/new_mexico.png',
-  'new york': 'assets/images/new_york.png',
-  'north carolina': 'assets/images/north_carolina.png',
-  'north dakota': 'assets/images/north_dakota.png',
-  'ohio': 'assets/images/ohio.png',
-  'oklahoma': 'assets/images/oklahoma.png',
-  'oregon': 'assets/images/oregon.png',
-  'pennsylvania': 'assets/images/pennsylvania.png',
-  'rhode island': 'assets/images/rhode_island.png',
-  'south carolina': 'assets/images/south_carolina.jpg',
-  'south dakota': 'assets/images/south_dakota.png',
-  'tennessee': 'assets/images/tennessee.png',
-  'texas': 'assets/images/texas.png',
-  'utah': 'assets/images/utah.png',
-  'vermont': 'assets/images/vermont.png',
-  'virginia': 'assets/images/virginia.png',
-  'washington': 'assets/images/washington.png',
-  'west virginia': 'assets/images/west_virginia.png',
-  'wisconsin': 'assets/images/wisconsin.png',
-  'wyoming': 'assets/images/wyoming.png'
-  };
-
-var stateObj = {
-  alabama: {
+  0: {
     name: 'alabama',
     image: 'assets/images/alabama.png'
-  }
+  },
+  1: {
+    name: 'alaska',
+    image: 'assets/images/alaska.png'
+  },
+  2: {
+    name: 'arizona',
+    image: 'assets/images/arizona.png'
+  },
+  3: {
+    name: 'arkansas',
+    image: 'assets/images/arkansas.png'
+  },
+  4: {
+    name: 'california',
+    image: 'assets/images/california.png'
+  },
+  5: {
+    name: 'colorado',
+    image: 'assets/images/colorado.png'
+  },
+  6: {
+    name: 'connecticut',
+    image: 'assets/images/connecticut.gif'
+  },
+  7: {
+    name: 'deleware',
+    image: 'assets/images/deleware.png'
+  },
+  8: {
+    name: 'florida',
+    image: 'assets/images/florida.png'
+  },
+  9: {
+    name: 'georgia',
+    image: 'assets/images/georgia.png'
+  },
+  10: {
+    name: 'hawaii',
+    image: 'assets/images/hawaii.png'
+  },
+  11: {
+    name: 'idaho',
+    image: 'assets/images/idaho.png'
+  },
+  12: {
+    name: 'illinois',
+    image: 'assets/images/illinois.png'
+  },
+  13: {
+    name: 'indiana',
+    image: 'assets/images/indiana.png'
+  },
+  14: {
+    name: 'iowa',
+    image: 'assets/images/iowa.png'
+  },
+  15: {
+    name: 'kansas',
+    image: 'assets/images/kansas.gif'
+  },
+  16: {
+    name: 'kentucky',
+    image: 'assets/images/kentucky.png'
+  },
+  17: {
+    name: 'louisiana',
+    image: 'assets/images/louisiana.png'
+  },
+  18: {
+    name: 'maine',
+    image: 'assets/images/maine.png'
+  },
+  19: {
+    name: 'maryland',
+    image: 'assets/images/maryland.png'
+  },
+  20: {
+    name: 'massachusetts',
+    image: 'assets/images/massachusetts.png'
+  },
+  21: {
+    name: 'michigan',
+    image: 'assets/images/michigan.png'
+  },
+  22: {
+    name: 'minnesota',
+    image: 'assets/images/minnesota.png'
+  },
+  23: {
+    name: 'mississippi',
+    image: 'assets/images/mississippi.png'
+  },
+  24: {
+    name: 'missouri',
+    image: 'assets/images/missouri.png'
+  },
+  25: {
+    name: 'montana',
+    image: 'assets/images/montana.png'
+  },
+  26: {
+    name: 'nebraska',
+    image: 'assets/images/nebraska.png'
+  },
+  27: {
+    name: 'nevada',
+    image: 'assets/images/nevada.png'
+  },
+  28: {
+    name: 'new hampshire',
+    image: 'assets/images/new_hampshire.gif'
+  },
+  29: {
+    name: 'new jersey',
+    image: 'assets/images/new_jersey.png'
+  },
+  30: {
+    name: 'new mexico',
+    image: 'assets/images/new_mexico.png'
+  },
+  31: {
+    name: 'new york',
+    image: 'assets/images/new_york.png'
+  },
+  32: {
+    name: 'north carolina',
+    image: 'assets/images/north_carolina.png'
+  },
+  33: {
+    name: 'north dakota',
+    image: 'assets/images/north_dakota.png'
+  },
+  34: {
+    name: 'ohio',
+    image: 'assets/images/ohio.png'
+  },
+  35: {
+    name: 'oklahoma',
+    image: 'assets/images/oklahoma.png'
+  },
+  36: {
+    name: 'oregon',
+    image: 'assets/images/oregon.png'
+  },
+  37: {
+    name: 'pennsylvania',
+    image: 'assets/images/pennsylvania.png'
+  },
+  38: {
+    name: 'rhode island',
+    image: 'assets/images/rhode_island.png'
+  },
+  39: {
+    name: 'south carolina',
+    image: 'assets/images/south_carolina.jpg'
+  },
+  40: {
+    name: 'south dakota',
+    image: 'assets/images/south_dakota.png'
+  },
+  41: {
+    name: 'tennessee',
+    image: 'assets/images/tennessee.png'
+  },
+  42: {
+    name: 'texas',
+    image: 'assets/images/texas.png'
+  },
+  43: {
+    name: 'utah',
+    image: 'assets/images/utah.png'
+  },
+  44: {
+    name: 'vermont',
+    image: 'assets/images/vermont.png'
+  },
+  45: {
+    name: 'virginia',
+    image: 'assets/images/virginia.png'
+  },
+  46: {
+    name: 'washington',
+    image: 'assets/images/washington.png'
+  },
+  47: {
+    name: 'west virginia',
+    image: 'assets/images/west_virginia.png'
+  },
+  48: {
+    name: 'wisconsin',
+    image: 'assets/images/wisconsin.png'
+  },
+  49: {
+    name: 'wyoming',
+    image: 'assets/images/wyoming.png'
+  },
+  length: 50
 }
 
-console.log(stateObj.alabama.image)
+function newWord() {
+  var num = Math.floor(Math.random() * 50);
+  console.log(states[num]);
+}
+// turn object into an array of objects. Remove the property names and use the name property to label them
